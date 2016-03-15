c0b65f969139263600408fc811d90f0d78ecdb03
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-14 22:00:07 -0500

    Not sure why it thinks it will accept numbers less than 0

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -26,23 +26,23 @@ def valid_date?(month, day, year)
     year_chk = true
   end
 
-if month >= 1 && month <= 12
-  month_chk = true
-end
+  if month >= 1 && month <= 12
+    month_chk = true
+  end
 
-if month = 1 || month = 3 || month = 5 || month = 7 || month = 8 || month = 10 || month = 12
-  month_days = 31
-elsif month = 4 || month = 6 || month = 9 || month = 11
-  month_days = 30
-else month = 2
-  month_days = february
-end
+  if month = 1 || month = 3 || month = 5 || month = 7 || month = 8 || month = 10 || month = 12
+    month_days = 31
+  elsif month = 4 || month = 6 || month = 9 || month = 11
+    month_days = 30
+  else month = 2
+    month_days == february
+  end
 
-if day >= 1 && day <= month_days
-  day_chk = true
-end
+  if day >= 1 && day <= month_days
+    day_chk = true
+  end
 
-if month_chk = true && day_chk = true && year_chk = true
-  return true
-end
+  if month_chk = true && day_chk = true && year_chk = true
+    return true
+  end
 end
