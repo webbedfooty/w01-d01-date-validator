4650e2a43ad7c692abc929369aef2cde35c37336
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-15 16:05:53 -0500

    Success

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -18,11 +18,11 @@ def valid_date?(month, day, year)
   end
 
   month_days = 31
-  if month = 1 || month = 3 || month = 5 || month = 7 || month = 8 || month = 10 || month = 12
+  if month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12
     month_days = 31
-  elsif month = 4 || month = 6 || month = 9 || month = 11
+  elsif month == 4 || month == 6 || month == 9 || month == 11
     month_days = 30
-  else month = 2
+  elsif month == 2
     month_days = february
   end
 
