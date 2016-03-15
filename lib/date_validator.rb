5034106c14653e4660cc10e4b51b4ac2e3007432
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-14 21:39:50 -0500

    Closer - fixed error

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -30,7 +30,7 @@ if month <= 12
   month_chk = true
 end
 
-if month = 1 || month = 3 || month = 5 || month = 7 || month = || 8 || month = 10 || month = 12
+if month = 1 || month = 3 || month = 5 || month = 7 || month = 8 || month = 10 || month = 12
   month_days = 31
 elsif month = 4 || month = 6 || month = 9 || month = 11
   month_days = 30
