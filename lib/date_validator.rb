03b16d5a7e3034bc17b17d1bfc182a1aed4da7d5
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-20 14:43:45 -0500

    with TomDocs comments

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -1,31 +1,56 @@
 # Given a month, day, and year, validate whether or not
 # the resulting date is within a given set of parameters.
 #
-# Step 1: determine if the year falls between the range 1880-2280, inclusive
+# Step 1: determine if the year falls within the range 1880-2280, inclusive
 # Step 2: determine if the month is a valid entry
-# Step 3: determine the number of days based on the month and year submitted
-# Step 4: determine if the year is a leap year
+# Step 3: determine if the year is a leap year
 #   and if so, assign the number of days for February at 29
+# Step 4: determine the number of days based on the month and year submitted
 # Step 5: determine if the day submitted falls within that determined range
-
+#######################################
 def valid_date?(month, day, year)
   year_valid(year) &&
   month_valid(month) &&
-  day_valid(day)
+  day_valid(day, month, year)
 end
-
-# Step 1: determine if the year falls within the range 1880-2280, inclusive
+#######################################
+# Step 1: determine if the YEAR falls within the range 1880-2280, inclusive
+#
+# + YEAR: integer representation of year (e.g. 1999 = year 1999)
+#
+# Returns true if YEAR is valid; otherwise returns false
 def year_valid(year)
-  if year > 1879 && year < 2281
+  year >= 1880 && year <= 2280
 end
-
-# Step 2: determine if the month is a valid entry
+#######################################
+# Step 2: determines if the MONTH is a valid entry
+#
+# + MONTH: integer representation of month (e.g. January = 1)
+#
+# Returns true if MONTH is valid; otherwise returns false
 def month_valid(month)
-  month > 0 && month < 13
+  month >= 1 && month <= 12
 end
-
-# Step 3: determine the number of days based on the month and year submitted
-
+#######################################
+# Step 3: determine if the year is a LEAP YEAR,
+# and if so, assign the number of days for February at 29
+#
+# + FEBRUARY: integer representation for the number of days in February
+#
+# Returns 29 if it is LEAP YEAR; otherwise 28 if not a LEAP YEAR
+def february(year)
+  if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+    february = 29
+  else
+    february = 28
+end
+#######################################
+# Step 4: determine the number of days based on the month and year submitted
+#
+# + DAYS IN MONTH: integer representation of the number of days in the month
+#
+# Returns a value for 'month_days' based on the MONTH and YEAR provided;
+# the value for FEBRUARY is determined on whether or not it is a LEAP YEAR
 def month_days(month, year)
   month_days = {
     1 => 31,
@@ -45,20 +70,12 @@ def month_days(month, year)
 
   month_days[month]
 end
-
-# Step 4: determine if the year is a leap year,
-# and if so, assign the number of days for February at 29
-
-def february(year)
-  if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
-    february = 29
-  else
-    february = 28
-end
-
-# Step 5: determine if the day submitted falls within that determined range
-def day_valid(day, month_days)
-  day > 0 && day <= month_days
-end
-
+#######################################
+# Step 5: determine if the DAY submitted falls within that determined range
+#
+# + DAY: integer representation of the day (e.g. 4 = 4th)
+#
+# Returns true if DAY is valid; otherwise returns false
+def day_valid(day, month_days, year)
+  day >= 1 && day <= month_days
 end
