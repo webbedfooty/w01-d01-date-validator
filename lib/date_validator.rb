ec9b771cdb561d10245f255cc5501f2d73b10e59
Patrick Wiechert <patrickwiechert@yahoo.com>
2016-03-20 13:25:12 -0500

    almost done refactoring

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -10,52 +10,56 @@
 # Step 6: determine if all 3 factors are true, given the set of parameters
 
 def valid_date?(month, day, year)
-valid_date = true
+  year_valid(year) &&
+  month_valid(month) &&
+  day_valid(day)
+end
 
-# Step 2: determine if the year falls within the range 1880-2280, inclusive
-#  year_chk = false
-  if year < 1880 || year > 2280
-    valid_date = false
-  else
-  end
+# Step : determine if the year falls within the range 1880-2280, inclusive
+def year_valid(year)
+  if year > 1879 && year < 2281
+end
 
-# Step 3: determine if the month is a valid entry
-#  month_chk = false
-  if month < 1 || month > 12
-    valid_date = false
-  else
-  end
+# Step : determine if the month is a valid entry
+def month_valid(month)
+  month > 0 && month < 13
+end
 
-# Step 1: determine if the year is a leap year,
-#   and if so, assign the number of days for February at 29
-  february = 28
+# Step : determine the number of days based on the month and year submitted
+
+def month_days (month, year)
+  month_days = {
+    1 => 31
+    2 => february(year)
+    3 => 31
+    4 => 30
+    5 => 31
+    6 => 30
+    7 => 31
+    8 => 31
+    9 => 30
+    10 => 31
+    11 => 30
+    12 => 31
+  }
+  month_days.default = 0
+
+  month_days[month]
+end
+
+# Step : determine if the year is a leap year,
+# and if so, assign the number of days for February at 29
+
+def february(year)
   if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
     february = 29
   else
-  end
+    february = 28
+end
 
-# Step 4: determine the number of days based on the month and year submitted
-  month_days = 31
-  if month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12
-    month_days = 31
-  elsif month == 4 || month == 6 || month == 9 || month == 11
-    month_days = 30
-  elsif month == 2
-    month_days = february
-  end
-
-# Step 5: determine if the day submitted falls within that determined range
-#  day_chk = false
-  if day < 1 || day > month_days
-      valid_date = false
-  else
-  end
-
-# Step 6: determine if all 3 factors are true, given the set of parameters
-#  if month_chk == true && day_chk == true && year_chk == true
-#    return true
-#  else
-#  end
-valid_date
+# Step : determine if the day submitted falls within that determined range
+def day_valid(day, month_days)
+  day > 0 && day <= month_days
+end
 
 end
